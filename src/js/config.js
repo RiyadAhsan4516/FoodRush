d9f5d1f0a41d051dc993b1fadf67cc7683fb75c6
Riyad Ahsan <riyad.ahsan4516@gmail.com>
2021-09-02 22:05:00 +0600

    CSE470 PROJECT

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '15de4d61-9237-4275-85ae-19ff4f5202f2';
+export const MODAL_CLOSE_SEC = 2.5;
